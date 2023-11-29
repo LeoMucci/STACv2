8095efad1775b6520c89fe6f2f9fd07d78fc41a0
Leo Mucci <leocapramucci@gmail.com>
2023-11-29 11:22:16 -0300

    css esqsenha

--- Login/EsqueciSenha/redefinirsenha.php
+++ Login/EsqueciSenha/redefinirsenha.php
@@ -1,4 +1,18 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Redefinir Senha</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
+
+<body>
+
+
+
+
+    <?php
 $conexao = mysqli_connect("localhost", "root", "", "bd");
 
 if ($conexao == FALSE) {
@@ -15,13 +29,34 @@ $result = mysqli_query($conexao, $query);
 
 if ($result) {
     if (mysqli_num_rows($result) == 1) {
-        // Exibir um formulário para inserir uma nova senha
-        echo '<form action="atualizarsenha.php" method="post">
-                <label for="nova_senha">Nova Senha:</label>
-                <input type="password" name="nova_senha" required>
-                <input type="hidden" name="token" value="'.$token.'">
-                <button type="submit">Atualizar Senha</button>
-            </form>';
+    ?>
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 offset-md-4 form">
+
+
+
+
+
+                <form action="atualizarsenha.php" method="post" class="esqsenha">
+
+                    <img src="../images/aLogo Stac.png" id="icon" alt="logo" />
+
+                    <div class="form-group">
+                        <b><label for="nova_senha" style="font-size:large;" >Nova Senha:</label></b>
+                    <input type="password" class="form-control" name="nova_senha" placeholder="Insira a Nova Senha" required>
+                    <input type="hidden" name="token" value="'.$token.'">
+                    <br>
+                    <button type="submit" class="form-control button">Atualizar Senha</button>
+                </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+    <?php
     } else {
         echo "Token inválido.";
     }
@@ -31,3 +66,7 @@ if ($result) {
 
 mysqli_close($conexao);
 ?>
+
+</body>
+
+</html>
